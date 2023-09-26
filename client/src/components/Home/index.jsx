0889b528ca9a1d886cdc6c29586e02f6c65d7de8
shioryyy <xqf0103@bu.edu>
2023-09-25 23:14:55 -0400

    home page navigation

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styles from './home.module.css'
+import { Link } from 'react-router-dom' 
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img src="/public/logo.jpg" alt="Your Logo" className={styles.logo} />
+      </div>
+      <div className={styles.linksContainer}>
+        <Link to="/register" className={styles.link}>Join in</Link>
+        <Link to="/login" className={styles.link}>Sign up</Link>
+      </div>
     </div>
   )
 }
