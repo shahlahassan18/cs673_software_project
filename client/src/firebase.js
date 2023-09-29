7a43220efd1df95f74d3860b07bd24449ff80f85
shahlahassan123 <ahlan.shahla@gmail.com>
2023-09-29 18:43:19 -0500

    updated firebaseconfig

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,16 +3,25 @@ import {getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult} from
 import {getStorage} from 'firebase/storage'
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAkCHdMSxL7_MqRubmKcsF60m1HP0B0upo",
-    authDomain: "software-engineering-pro-3040f.firebaseapp.com",
-    projectId: "software-engineering-pro-3040f",
-    storageBucket: "software-engineering-pro-3040f.appspot.com",
-    messagingSenderId: "144079595073",
-    appId: "1:144079595073:web:7330219a753b156767c8c0",
-    measurementId: "G-ZPEKWLZZ78"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAkCHdMSxL7_MqRubmKcsF60m1HP0B0upo",
+//     authDomain: "software-engineering-pro-3040f.firebaseapp.com",
+//     projectId: "software-engineering-pro-3040f",
+//     storageBucket: "software-engineering-pro-3040f.appspot.com",
+//     messagingSenderId: "144079595073",
+//     appId: "1:144079595073:web:7330219a753b156767c8c0",
+//     measurementId: "G-ZPEKWLZZ78"
+//   };
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCGQM0_diw6g0DL1t8mhAjLl1UR6lVdqc4",
+  authDomain: "softwareengineeringproje-ce511.firebaseapp.com",
+  projectId: "softwareengineeringproje-ce511",
+  storageBucket: "softwareengineeringproje-ce511.appspot.com",
+  messagingSenderId: "539519997261",
+  appId: "1:539519997261:web:11a7ff83a73b35d8321304",
+  measurementId: "G-8EJL2PFGMK"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
