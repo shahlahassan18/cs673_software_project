8c7c1657a7e074578bbfd6e9a488f20f61fa263b
shahlahassan123 <ahlan.shahla@gmail.com>
2023-10-10 20:21:01 -0400

    Banner images

--- client/src/components/Feed/index.jsx
+++ client/src/components/Feed/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AddPost from '../AddPost'
+import Banner from '../Banner'
 import Posts from '../Posts'
 import styles from "./feed.module.css"
 
 const Feed = () => {
   return (
     <div className={styles.feed}>
+      <Banner />
       <AddPost />
       <Posts />
     </div>
